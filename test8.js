73e21f4aa94e437bee311034b532a41eb2e217ee
AMJAD MM <166852555+amjad-mm@users.noreply.github.com>
2026-06-04 11:16:41 +0530

    Add files via upload

---
+++ test8.js
@@ -0,0 +1,16 @@
+//this is the destrcturing we can easily extract vlue form the object and array
+
+let arr =["red","blue","yellow"]
+let [first,second]=arr
+console.log(first)
+console.log(second)
+
+const obj ={
+    name :"amjad",
+    age :19,
+    place:"manjeri"
+
+}
+
+let {name, age}=obj
+console.log(name,age)
